f8f0122297cecdee3922f55bb0b992c7e611677e
André Pessoa <pessoam.andre@gmail.com>
2018-08-28 22:17:16 -0300

    Form validate & Comment List

--- scripts/script.js
+++ scripts/script.js
@@ -37,6 +37,15 @@
                     return json;
                 })
         },
+        loadComments: function(postId){
+            var self = this;
+            var url = postId ? 'https://jsonplaceholder.typicode.com/posts/' + postId + '/comments' : 'https://jsonplaceholder.typicode.com/comments' ;
+            return fetch(url)
+                .then(function(response){ return response.json()})
+                .then(function(json){
+                    Global.comments = json;
+                });
+        },
         addComment: function(data){
             return fetch('https://jsonplaceholder.typicode.com/comments', {
                     method: 'POST',
@@ -118,6 +127,12 @@
                 Nav.goTo( target );
                 return false;
             });
+            this.$el.on('click', 'a[href="#listcomments"]', function(){
+                var target = $(this).attr("href");
+                var postId = $(this).data('postId');
+                CommentsList.update(postId);
+                Nav.goTo( target );
+            })
         },
         renderTable: function(posts){
             posts = posts || Global.posts;
@@ -139,7 +154,60 @@
             var self = this;
 
             this.$el.addClass('loading');
+            this.$table.empty();
             Promise.all([Global.loadUsers(), Global.loadPosts()]).then(function(values) {
+                self.renderTable();
+                self.$el.removeClass('loading');
+            });
+        }
+    }
+
+    var CommentsList = {
+        $el:null,
+        $table: null,
+        $newLine: null,
+        init: function( selectorSection ){
+            this.$el  = $(selectorSection);
+            this.$table = this.$el.find('tbody');
+            this.$newLine = this.$el.find('tbody tr');
+            this.$newLine.detach();
+            this.currentPostId = undefined;
+
+            this.addEvents();
+            this.update();
+        },
+        addEvents: function(){
+            var self = this;
+            this.$el.on('click', 'a[href="#newcomment"]', function(){
+                var target = $(this).attr("href");
+                CommentForm.update( self.currentPostId );
+                Nav.goTo( target );
+                return false;
+            });
+        },
+        renderTable: function(comments){
+            comments = comments || Global.comments;
+            this.$table.empty();
+
+            for (var i = 0; i < comments.length; i++) {
+                var comment = comments[i];
+                var $line = this.$newLine.clone();
+                $line.find('td').eq(0).html(comment.id);
+                $line.find('td').eq(1).html(comment.postId);
+                $line.find('td').eq(2).html(comment.name);
+                $line.find('td').eq(3).html(comment.email);
+                $line.find('a').data('postId', comment.id);
+
+                this.$table.append($line);
+            }
+        },
+        update: function(postId){
+            this.currentPostId = postId;
+            var self = this;
+
+            this.$el.addClass('loading');
+            this.$table.empty();
+            Promise.all([Global.loadUsers(), Global.loadComments(postId)]).then(function(values) {
                 self.renderTable();
                 self.$el.removeClass('loading');
             });
@@ -176,7 +244,21 @@
             });
         },
         reset: function(){
+            this.$el.find('.feedback').html('');
             this.$form.find('[name]').val('');
+        },
+        validate: function( data, user ){
+            var erros = []; 
+            if( !data.title ) erros.push('PostId is obligatory');
+            if( !data.userId ) erros.push('Author is obligatory');
+            if( !data.body ) erros.push('Content is obligatory');
+
+            if( erros.length ){
+                erros.unshift('<strong>Please, verify the form:</strong>');
+                this.$el.find('.feedback').html( erros.join('<br>') );
+            }
+
+            return erros.length;
         },
         submit: function(){
             var data = {
@@ -186,11 +268,15 @@
             };
             var self = this;
 
-            Global.addPost(data).then(function(){
-                PostsList.update();
-                Nav.goTo('#list');
-                self.reset();
-            });
+            var erros = this.validate( data );
+
+            if( erros === 0 ){
+                Global.addPost(data).then(function(){
+                    PostsList.update();
+                    Nav.goTo('#list');
+                    self.reset();
+                });
+            }
         }
     }
 
@@ -199,6 +285,7 @@
         init: function( selectorSection ){
             this.$el   = $(selectorSection);
             this.$form = this.$el.find('form');
+            this.currentPostId = undefined;
 
             this.addEvents();
             this.update();
@@ -211,6 +298,8 @@
             });
         },
         update: function( postId ){
+            this.currentPostId = postId;
+
             var $userSelect = this.$form.find('[name="author"]');
             Global.loadUsers().then(function(){
                 $userSelect.find('[value]').remove();
@@ -226,11 +315,27 @@
             this.$form.find('[name="post"]').val(postId);
         },
         reset: function(){
+            this.$el.find('.feedback').html('');
             this.$form.find('[name]').val('');
+        },
+        validate: function( data, user ){
+            var erros = []; 
+            if( !data.postId ) erros.push('PostId is obligatory');
+            if( data.postId && !Global.posts[data.postId] ) erros.push('Invalid PostId');
+            if( !user ) erros.push('Author is obligatory');
+            if( !data.body ) erros.push('Content is obligatory');
+            if( user && !user.name ) erros.push('Invalid author. Please select another.');
+
+            if( erros.length ){
+                erros.unshift('<strong>Please, verify the form:</strong>');
+                this.$el.find('.feedback').html( erros.join('<br>') );
+            }
+
+            return erros.length;
         },
         submit: function(){
             var userId = this.$form.find('[name="author"]').val();
-            var user = Global.users[userId];
+            var user = Global.users[userId] || { name: '', email: '' } ;
 
             var data = {
                 postId: this.$form.find('[name="post"]').val(),
@@ -240,11 +345,15 @@
             };
             var self = this;
 
-            Global.addComment(data).then(function(){
-                PostsList.update();
-                Nav.goTo('#list');
-                self.reset();
-            });
+            var erros = this.validate( data, user );
+
+            if( erros === 0 ){
+                Global.addComment(data).then(function(){
+                    CommentsList.update();
+                    Nav.goTo('#list');
+                    self.reset();
+                });
+            }
         }
     }
 
@@ -252,6 +361,7 @@
     $(function(){
         Nav.init('main', 'nav');
         PostsList.init('#list');
+        CommentsList.init('#listcomments');
         PostForm.init('#newpost');
         CommentForm.init('#newcomment');
     });
